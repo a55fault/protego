599cb86c6027c8e9470477dd5daad41158432d51
a55fault <prorock7@ya.ru>
2022-06-28 00:25:14 +0300

    new func

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 import { uiElements } from "./view.js";
 
 function MassCalculation() {
-    //console.log("click")
+    console.log("click")
     let { InputCount, InputMass, InputNeedCount, InputPercent, ButtonDo, Output, UsePercent, } = uiElements
     let result = InputMass.value * InputNeedCount.value / InputCount.value
     //console.log(typeof result, result)
     if (UsePercent.checked) {
         let resultPlus = result + result * InputPercent.value * 0.01;
-        Output.value = resultPlus.toFixed(2)+" g";
+        Output.value = resultPlus.toFixed(2) + " g";
     }
     else {
-        Output.value = result.toFixed(2)+" g"
+        Output.value = result.toFixed(2) + " g"
     }
 }
 
+function CountCalculation() {
+    console.log("click2")
+    let { InputCount, InputMass, InputNeedCount, InputPercent, ButtonDo, Output, UsePercent, InputGroupCount, InputGroupMass, InputAllMass, ButtonDoCount, Output2, } = uiElements
+    let result = InputGroupCount.value * InputAllMass.value / InputGroupMass.value
+    //console.log(typeof result, result)
+    Output2.value = result.toFixed(2) + " шт"
+}
+
 uiElements.ButtonDo.onclick = MassCalculation;
+uiElements.ButtonDoCount.onclick = CountCalculation;
--- view.js
+++ view.js
@@ -6,4 +6,9 @@ export let uiElements = {
     ButtonDo: document.querySelector(".ButtonDo"),
     Output: document.querySelector(".output"),
     UsePercent: document.querySelector(".UsePercent"),
+    InputGroupCount: document.querySelector(".InputGroupCount"),
+    InputGroupMass: document.querySelector(".InputGroupMass"),
+    InputAllMass: document.querySelector(".InputAllMass"),
+    ButtonDoCount: document.querySelector(".ButtonDoCount"),
+    Output2: document.querySelector(".output2"),
 }
